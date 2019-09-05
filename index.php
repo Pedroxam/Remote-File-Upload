aa3ef0c1776624d5fa54356543f157ba568e551e
Amoo Pedram <Pedroxam@gmail.com>
2019-09-05 23:12:42 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,92 @@
+<?php
+/*
+ * By Pedram [ Pedroxam ]
+*/
+
+// Replace your server url and remote.php file
+define('SERVER_API','http://example.com/remote.php');
+?>
+
+<!doctype html>
+<html lang="en-US">
+<head>
+	<meta charset="utf-8">
+	<title>Remote Upload File</title>
+	<link rel="stylesheet" type="text/css" href="./style.css">
+	<script src="./jquery.min.js"></script>
+</head>
+<body>
+	<div id="upload">
+		<h2>Upload Now !</h2>
+		<form id="remote">
+			<fieldset>
+				<p><span class="fontawesome-link"></span><label for="link">Link:</label></p>
+				<p><input type="text" id="url" name="url" placeholder="http://" required autofocus></p>
+				<p><input type="text" id="name" name="name" placeholder="filename" required autofocus></p>
+				<p><input type="submit" value="Upload" name="send" class="Button"></p>
+			</fieldset>
+		</form>
+		<div id="result">
+			Progress: <span class="progress"></span>
+			<br/>
+			Uploaded: <span class="uploaded"></span>
+			<br/>
+			Filesize: <span class="filesize"></span>
+		</div>
+	</div>
+	
+<script>
+
+	var doProgress;
+		
+	function getProgress(){
+		$.ajax({
+			url: '<?= SERVER_API; ?>?progress=1',
+			dataType: 'json',
+			type: 'POST',
+			data: {
+				name: $('#name').val()
+			},
+			success:function(data){
+				$('#result .progress').html(data.progress + ' %');
+				$('#result .uploaded').html(data.uploaded);
+				$('#result .filesize').html(data.size);
+				if(data.progress >= 100){
+					clearInterval(doProgress);
+					$('#result').html('File Succesfully Uploaded!');
+				}
+			},
+		})
+	}
+
+	$(document).ready(function(){
+		$('#remote').submit(function(e){
+		  e.preventDefault();
+			$.ajax({
+				url: '<?php echo SERVER_API; ?>',
+				dataType: 'json',
+				type: 'POST',
+				data: $(this).serialize(),
+				beforeSend:function()
+				{
+					$('.Button').attr('disabled',true);
+					doProgress = setInterval(getProgress, 2500);
+				},
+				success:function(){
+					$('.Button').attr('disabled',false);
+					
+					if(data.status){
+						$('#result').html('File Succesfully Uploaded.');
+					}
+					else {
+						$('#result').html('Sorry, Please Check your server config.');
+					}
+				},
+			})
+		});
+	});
+	
+</script>
+
+</body>
+</html>
---
+++ remote.php
@@ -0,0 +1,152 @@
+<?php
+/*
+ * By Pedram [ Pedroxam ]
+*/
+header('Access-Control-Allow-Origin: *');
+header('content-type: application/json; charset=utf-8');
+
+error_reporting(0);
+set_time_limit(0);
+
+/*
+ * Large download file
+*/
+function largeDownload($url,$path){
+    $fp = fopen($path, 'w');
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_FILE, $fp);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    fclose($fp);
+	return $data;
+}
+
+/*
+ * Get the remote file size
+*/
+function remote_filesize($url) {
+    static $regex = '/^Content-Length: *+\K\d++$/im';
+    if (!$fp = @fopen($url, 'rb')) {
+        return false;
+    }
+    if (
+        isset($http_response_header) &&
+        preg_match($regex, implode("\n", $http_response_header), $matches)
+    ) {
+        return (int)$matches[0];
+    }
+  	  return strlen(stream_get_contents($fp));
+}
+
+/*
+ * Readable file size
+*/
+function formatSizeUnits($bytes)
+{
+        if ($bytes >= 1073741824)
+        {
+            $bytes = number_format($bytes / 1073741824, 2) . ' GB';
+        }
+        elseif ($bytes >= 1048576)
+        {
+            $bytes = number_format($bytes / 1048576, 2) . ' MB';
+        }
+        elseif ($bytes >= 1024)
+        {
+            $bytes = number_format($bytes / 1024, 2) . ' KB';
+        }
+        elseif ($bytes > 1)
+        {
+            $bytes = $bytes . ' Bytes';
+        }
+        elseif ($bytes == 1)
+        {
+            $bytes = $bytes . ' Bytes';
+        }
+        else
+        {
+            $bytes = '0 Bytes';
+        }
+
+        return $bytes;
+}
+
+function getExtention($name)
+{
+	return substr(basename($name),-3);
+}
+
+/*
+ * Get the uploaded progress percentage
+*/
+function getProgress($name)
+{
+	$log  = str_replace('=','',base64_encode(substr($name,0,5)));
+	
+	$total_size = file_get_contents($log . '.txt');
+	
+	$current_size = filesize($name);
+	
+	$progress = ( 100 * $current_size ) / $total_size;
+	$progress = floor($progress);
+
+	return json_encode(array(
+		'progress'  => $progress,
+		'uploaded'  => formatSizeUnits($current_size),
+		'size' 	    => formatSizeUnits($total_size)
+	));
+}
+
+/*
+ Start download file
+*/
+function downloadFile($url,$name)
+{
+	$url = urldecode($url);
+	
+	$status = true;
+	
+	$put = file_put_contents(str_replace('=','',base64_encode(substr($name,0,5))).'.txt',remote_filesize($url));
+	
+	if(!$put){
+		$status = false;
+		return;
+	}
+		
+	largeDownload($url, $name);
+	
+	return json_encode(
+		array(
+			'status'   => $status,
+			'filename' => $name
+		)
+	);
+}
+
+/*
+ * Get the file progress
+*/
+if(isset($_REQUEST['progress']) && !empty($_REQUEST['progress'])){
+	exit(getProgress(trim($_REQUEST['name'])));
+}
+
+$url  = trim($_REQUEST['url']); // File url
+$name = trim($_REQUEST['name']); // File name
+
+if(!isset($url) && empty($name)) die();
+
+if(!filter_var($url, FILTER_VALIDATE_URL)) {
+	die();
+}
+
+$check_ext = getExtention($url,-3);
+$allowed = ['mkv','mp4','3gp','avi','m4v','mp3','flv','ts']; // Alowed file types
+
+/*
+ * Download File
+*/
+if(in_array($check_ext,$allowed)){
+	exit(downloadFile($url,basename($name)));
+}
+
+?>
